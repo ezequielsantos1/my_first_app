bbbd241435cc55b6ccd5b966572c637918978cf1
Ezequiel Keslley Viana Santos da Cruz <ezequielcx14@gmail.com>
2026-05-26 16:44:43 -0300

    feat(lib): adicionar um novo metodo de butão

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -54,31 +60,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
             const Text('Você apertou essa quantidade de vezes no botão'),
@@ -102,7 +88,14 @@ class _MyHomePageState extends State<MyHomePage> {
             FloatingActionButton(
               onPressed: _decrementCounter,
               tooltip: 'decremento',
-              child: const Icon(Icons.remove),)
+              child: const Icon(Icons.remove),),
+
+          const SizedBox(width: 10),
+
+              FloatingActionButton(
+                onPressed: _resetCounter,
+                tooltip: 'zerar',
+                child: const Icon(Icons.ac_unit),)
         ],
       ),
     );
